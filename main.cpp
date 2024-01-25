26b98470be9736871e00fcab2d6d8068d9b8c2b1
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-01-25 09:59:00 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "bankaccount.h"
+
+
+
+// class Lcd{ //display
+
+
+
+// }
+
+class BankAccount{ //EGNA H + CPP filer
+
+};
+
+
+
+int main() {
+    calculate();
+    std::cout << "RAST STRAX" << std::endl;
+    return 0;
+}
+
+// TARGET = main.exe - timestamps
+// DEPENDENCIES : main.cpp - timestamps bankaccount.cpp - timestamps
+// COMMANDS: gcc main.cpp bankaccount.cpp -O
